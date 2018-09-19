507058631e79e524a730c42f6773c2cf08f8f64a
koofree <yongku84@gmail.com>
2018-09-19 17:36:57 +0900

    Add set autoindent to vimrc file

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@
 
 TAB_SIZE=4
 
-
+# Tab Size
 echo "set tabstop=${TAB_SIZE}" >> .vimrc
 echo "set shiftwidth=${TAB_SIZE}" >> .vimrc
 echo "set softtabstop=${TAB_SIZE}" >> .vimrc
 echo "set expandtab" >> .vimrc
+
+# Auto Indent
+echo "set autoindent" >> .vimrc
 
 rm ~/.vimrc
 cp `dirname '$0'`/.vimrc ~/.vimrc
