d4d9285377523ca5534489aec126f7152bae622c
koofree <yongku84@gmail.com>
2018-09-21 14:46:31 +0900

    add syntax Color' ' '

--- setup.sh
+++ setup.sh
@@ -10,9 +10,16 @@ echo "set tabstop=${TAB_SIZE}" >> .vimrc
 echo "set shiftwidth=${TAB_SIZE}" >> .vimrc
 echo "set softtabstop=${TAB_SIZE}" >> .vimrc
 echo "set expandtab" >> .vimrc
+echo "" >> .vimrc
 
 # Auto Indent
 echo "set autoindent" >> .vimrc
+echo "" >> .vimrc
+
+# Builtin Syntax Color
+echo "filetype plugin indent on" >> .vimrc
+echo "set term=builtin_ansi" >> .vimrc
+echo "syntax on" >> .vimrc
 
 rm ~/.vimrc
 cp `dirname '$0'`/.vimrc ~/.vimrc
