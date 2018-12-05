7d41f7544b287ce432db741b0127d4fa8dac563a
Koo Lee <yongku84@gmail.com>
2018-12-05 15:36:50 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,6 +21,20 @@ echo "filetype plugin indent on" >> .vimrc
 echo "set term=builtin_ansi" >> .vimrc
 echo "syntax on" >> .vimrc
 
+# Add Plugin Manager
+echo "call plug#begin('~/.vim/plugged')" >> .vimrc
+echo "Plug 'junegunn/vim-easy-align'" >> .vimrc
+echo "Plug 'https://github.com/junegunn/vim-github-dashboard.git'" >> .vimrc
+echo "Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'" >> .vimrc
+echo "Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }" >> .vimrc
+echo "Plug 'tpope/vim-fireplace', { 'for': 'clojure' }" >> .vimrc
+echo "Plug 'rdnetto/YCM-Generator', { 'branch': 'stable' }" >> .vimrc
+echo "Plug 'fatih/vim-go', { 'tag': '*' }" >> .vimrc
+echo "Plug 'nsf/gocode', { 'tag': 'v.20150303', 'rtp': 'vim' }" >> .vimrc
+echo "Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }" >> .vimrc
+echo "Plug '~/my-prototype-plugin'" >> .vimrc
+echo "call plug#end()" >> .vimrc
+
 rm ~/.vimrc
 cp `dirname '$0'`/.vimrc ~/.vimrc
 
